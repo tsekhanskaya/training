77117bdddb824d03f3706f950851a265a89eeaf5
tsekhanskaya <anutta2222@gmail.com>
2022-07-18 12:57:57 +0300

    Created format .yaml, finished work with the File class

--- task_2/app.rb
+++ task_2/app.rb
@@ -8,11 +8,6 @@ require 'csv'
 require_relative 'lib/file'
 require_relative 'lib/parse'
 
-file = File.new
-
-file.create_filename
-file.create_file
-
 def main
   argument = YAML.load_file('lib/arguments.yaml')
   File.create_file(argument['filename'])
